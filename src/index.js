aa5391fe08ddd4b8965b591a5d49c0fb0521304c
CPHR <sashayolo17@gmail.com>
2023-02-26 23:48:44 -0500

    General fixes

--- src/index.js
+++ src/index.js
@@ -1,51 +1,58 @@
-const { app, BrowserWindow, TouchBar, Menu, Tray, ipcMain } = require('electron')
-const axios = require('axios').default;
-const fs = require('fs');
-const expandTilde = require('expand-tilde');
+const {
+  app,
+  BrowserWindow,
+  TouchBar,
+  Menu,
+  Tray,
+  ipcMain,
+} = require("electron");
+const axios = require("axios").default;
+const fs = require("fs");
+const expandTilde = require("expand-tilde");
 const path = require("path");
 
-const { TouchBarLabel, TouchBarButton, TouchBarSpacer } = TouchBar
+const { TouchBarLabel, TouchBarButton, TouchBarSpacer } = TouchBar;
 
 const COLORS = {
-  'BTC': '#F6931B',
-  'ETH': '#5C78DF',
-  'SOL': '#8E6ED9',
-  'MATIC': '#7D43DB',
-  'BNB': '#E5AE0D',
-  'AVAX': '#DD3F3E',
-  'XRP': '#22292F',
-  'ADA': '#3569D0',
-  'DOGE': '#C7A85A',
-  'DOT': '#EF1A7E',
-  'SHIB': '#EC432D',
-  'NEAR': '#010001',
-  'ATOM': '#2E3246',
-  'FLOW': '#11EF8A',
-  'UNI': '#F30075',
+  BTC: "#F6931B",
+  ETH: "#5C78DF",
+  SOL: "#8E6ED9",
+  MATIC: "#7D43DB",
+  BNB: "#E5AE0D",
+  AVAX: "#DD3F3E",
+  XRP: "#22292F",
+  ADA: "#3569D0",
+  DOGE: "#C7A85A",
+  DOT: "#EF1A7E",
+  SHIB: "#EC432D",
+  NEAR: "#010001",
+  ATOM: "#2E3246",
+  FLOW: "#11EF8A",
+  UNI: "#F30075",
 };
 
 const btcButton = new TouchBarButton({
-  label: '₿',
-  backgroundColor: '#F6931B',
+  label: "₿",
+  backgroundColor: "#F6931B",
 });
 
 const getTickers = (tickersData) => {
-  tickersString = tickersData.replace(/\s/g, '');
-  return ['BTC'].concat(tickersString.split(',').filter(item => !!item));
+  tickersString = tickersData.replace(/\s/g, "");
+  return ["BTC"].concat(tickersString.split(",").filter((item) => !!item));
 };
 
 const getButtons = (tickers) => {
   let buttons = {
     btc: new TouchBarButton({
-      label: '₿',
+      label: "₿",
       backgroundColor: COLORS.BTC,
     }),
-  }
-  tickers.forEach(ticker => {
-    if (ticker !== 'BTC') {
+  };
+  tickers.forEach((ticker) => {
+    if (ticker !== "BTC") {
       buttons[ticker] = new TouchBarButton({
         label: ticker.toUpperCase(),
-        backgroundColor: COLORS[ticker.toUpperCase()] || '#808080',
+        backgroundColor: COLORS[ticker.toUpperCase()] || "#808080",
       });
     }
   });
@@ -53,19 +60,30 @@ const getButtons = (tickers) => {
 };
 
 const getCoinsPrice = async (tickers, buttons) => {
-  const res = await axios.get(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=${tickers.join(',')}`, {
-    headers: {
-      'X-CMC_PRO_API_KEY': 'e8b8e828-3aef-4f31-bd9e-6b886f1e72cf'
-    },
-  });
+  const res = await axios.get(
+    `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=${tickers.join(
+      ","
+    )}`,
+    {
+      headers: {
+        // Change value to your Coinmaret cap api key.
+        "X-CMC_PRO_API_KEY": "YOUR API KEY",
+      },
+    }
+  );
 
   const data = res?.data?.data;
   if (!data) return;
 
-  Object.keys(buttons).forEach(buttonTicker => {
+  Object.keys(buttons).forEach((buttonTicker) => {
     const buttonTickerUppercase = buttonTicker.toUpperCase();
-    if (data[buttonTickerUppercase] && data[buttonTickerUppercase][0].quote?.USD?.price) {
-      buttons[buttonTicker].label = `${buttonTickerUppercase} ${data[buttonTickerUppercase][0].quote?.USD?.price.toFixed(2)}$`;
+    if (
+      data[buttonTickerUppercase] &&
+      data[buttonTickerUppercase][0].quote?.USD?.price
+    ) {
+      buttons[buttonTicker].label = `${buttonTickerUppercase} ${data[
+        buttonTickerUppercase
+      ][0].quote?.USD?.price.toFixed(2)}$`;
     } else {
       buttons[buttonTicker].label = `${buttonTickerUppercase} No price`;
     }
@@ -76,7 +94,7 @@ let window;
 let tray;
 let interval;
 app.whenReady().then(() => {
-  ipcMain.on('getPrices', (event, data) => {
+  ipcMain.on("getPrices", (event, data) => {
     const reBuildCoinsButtons = () => {
       const tickers = getTickers(data.tickers);
       const buttons = getButtons(tickers);
@@ -84,24 +102,27 @@ app.whenReady().then(() => {
       getCoinsPrice(tickers, buttons);
       window.setTouchBar(null);
       window.setTouchBar(touchBar);
-    }
+    };
     clearInterval(interval);
     reBuildCoinsButtons();
-    interval = setInterval(() => {
-      reBuildCoinsButtons();
-    }, data.interval ? (data.interval * 60 * 1000) : 300000);
+    interval = setInterval(
+      () => {
+        reBuildCoinsButtons();
+      },
+      data.interval ? data.interval * 60 * 1000 : 300000
+    );
   });
   window = new BrowserWindow({
     width: 600,
     height: 420,
-    backgroundColor: '#ABABAB',
+    backgroundColor: "#ABABAB",
     title: "Coins Touchbar",
     resizable: false,
     disableHtmlFullscreenWindowResize: true,
     fullscreen: false,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-  window.loadFile('src/index.html');
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  window.loadFile("src/index.html");
 });
